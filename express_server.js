3a3635acb2f7efcdd61f6c9c36839b76dc539392
Delson Tan <delsonwtan@gmail.com>
2018-07-11 23:52:43 +0000

    Added users database(an object of objects), and fixed a fatal syntax error

--- express_server.js
+++ express_server.js
@@ -14,6 +14,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
 
 // GET: root address
 app.get("/", (req, res) => {
@@ -35,7 +47,7 @@ app.get("/u/:shortURL", (req, res) => {
 // GET: local address containing the registration page
 app.get("/register", (req, res) => {
   res.render("registration");
-}
+})
 
 // GET: local address containing URLs collection
 app.get("/urls", (req, res) => {
